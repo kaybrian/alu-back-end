3adffcf0a771c7022a7f968675338c0a0e9721c9
kaybrian <kayonbrian@gmail.com>
2023-02-20 14:27:20 +0200

    -pep8 fixes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,16 +10,19 @@ import sys
 if __name__ == '__main__':
     user_id = sys.argv[1]
     user = requests.request(
-        'GET', f'https://jsonplaceholder.typicode.com/users/{user_id}').json()
+        'GET', f'https://jsonplaceholder.typicode.com/users/{user_id}'
+        ).json()
     user_tasks = requests.request(
-        'GET', f'https://jsonplaceholder.typicode.com/users/{user_id}/todos/').json()
+        'GET', f'https://jsonplaceholder.typicode.com/users/{user_id}/todos/'
+        ).json()
 
     EMPLOYEE_NAME = user['name']
     NUMBER_OF_DONE_TASKS = len(
-        [x for x in user_tasks if x['completed'] == True])
+        [x for x in user_tasks if x['completed'] is True])
     TOTAL_NUMBER_OF_TASKS = len(user_tasks)
 
     print(
-        f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS})')
+        f'Employee {EMPLOYEE_NAME} is done with tasks(
+            {NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS})')
     [print("\t " + task["title"])
-     for task in [x for x in user_tasks if x['completed'] == True]]
+     for task in [x for x in user_tasks if x['completed'] is True]]
