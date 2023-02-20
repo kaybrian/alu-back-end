41d08458e9468d3060da9e1c092ace2206b55281
kaybrian <kayonbrian@gmail.com>
2023-02-20 18:59:37 +0200

    write to csv file

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    python script that returns TODO list progress for a given employee ID
+    and saves that information in a csv file
+"""
+
+# "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+
+import csv
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    USER_ID = sys.argv[1]
+    USER = requests.request(
+        "GET", f'https://jsonplaceholder.typicode.com/users/{USER_ID}/'
+        ).json()
+    USER_TASKS = requests.request(
+        "GET", f"https://jsonplaceholder.typicode.com/users/{USER_ID}/todos"
+        ).json()
+
+    with open(str(USER_ID)+".csv", "w", encoding="utf8") as file:
+        for task in USER_TASKS:
+            file.write('"' + str(USER_ID) + '"' +
+            "," + '"' + str(USER["username"]) + '"' +
+            ","+ '"' + str(task["completed"]) + '"'+
+            "," + '"' + str(task["title"])+ '"' + "\n")
